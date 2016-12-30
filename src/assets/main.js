fa060ef8a76e569445c08f6884d845cfcebfe282
Eric Peterson <eric@elpete.com>
2016-12-30 09:43:40 -0700

    Solution

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,21 @@
 $(function() {
 
   // your code will go here
-
+  $.ajax( {
+    url: "https://www.codeschool.com/users/elpete.json",
+    dataType: "jsonp",
+    success: function( response ) {
+      $( "#badges" ).html( response.courses.completed.map( completedCourse => {
+        return `
+          <div class="course">
+            <h3>${completedCourse.title}</h3>
+            <img src="${completedCourse.badge}" alt="${completedCourse.title}" />
+            <a href="${completedCourse.url}" target="_blank" class="btn btn-primary">
+              See Course
+            </a>
+          </div>
+        `
+      } ) );
+    }
+  } );
 });
